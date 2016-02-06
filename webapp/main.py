6556ddec5ac710ec74e804583cbca68d1abb8c33
rakou1986 <rakou1986@gmail.com>
2016-02-07 03:02:47 +0900

    DataTablesが必要とするstaticファイルが404となるのを防ぐ

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-from flask import Flask
+from flask import Flask, send_from_directory
 
 from views import player
 
 app = Flask(__name__)
 app.register_blueprint(player.bp, url_prefix="/player")
+
+@app.route("/images/<path:path>")
+def send_image(path):
+  """
+  jQueryのプラグインが画像を引くとき404になるのを防ぐツギハギ。
+  """
+  return send_from_directory("static/images", path)
+
 
 if __name__ == "__main__":
     app.debug = True
