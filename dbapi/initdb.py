c9176abf8c39b8443487a2a8be09264ea80403bb
rakou1986 <rakou1986@gmail.com>
2016-02-08 04:03:23 +0900

    TamahiyoCoreServiceオブジェクト構築の都合上importを遅らせる必要あり

--- dbapi/initdb.py
+++ dbapi/initdb.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 
 from models import *
-import migration
 
 def initialize():
   Base.metadata.drop_all(engine)
   Base.metadata.create_all(engine)
+  import migration
   migration.main()
 
 if __name__ == "__main__":
